eef0124b6f9aac18098b58344d732717a60604ab
Niranjan420 <niranjanrajesh02@gmail.com>
2020-07-27 16:45:19 +0530

    Banner 1.0

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function home() {
-  return <Hero hero="defaultHero"></Hero>;
+  return (
+    <Hero hero="defaultHero">
+      <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          Our Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
 
 Hero.defaultProps = {

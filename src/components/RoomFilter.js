efb6c64438a5b81e76a0c5341c1992ad613bcfe2
Niranjan420 <niranjanrajesh02@gmail.com>
2020-07-28 18:56:18 +0530

    All filters working

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -60,7 +60,7 @@ export default function RoomFilter({ rooms }) {
           </select>
         </div>
         {/*end select type*/}
-        {/*select type */}
+        {/*guests */}
         <div className="form-group">
           <label htmlFor="capacity">Guests</label>
           <select
@@ -73,7 +73,69 @@ export default function RoomFilter({ rooms }) {
             {peopleOptions}
           </select>
         </div>
-        {/*end select type*/}
+        {/*end guests*/}
+        {/* room price */}
+        <div className="form-group">
+          <label htmlFor="price">Room Price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        {/* end room price */}
+        {/* room size */}
+        <div className="form-group">
+          <label htmlFor="size">Room Size in sqFt</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* end room size */}
+        {/* extras */}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">Breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">Pets</label>
+          </div>
+        </div>
+        {/* end extras */}
       </form>
     </section>
   );

af22c3e82e85f7f84b15e0a79910934d361ff377
Niranjan420 <niranjanrajesh02@gmail.com>
2020-07-28 22:25:19 +0530

    App 1.0.1

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -65,7 +65,7 @@ export default class SingleRoom extends Component {
             <article className="info">
               <h3>Info</h3>
               <h6>price: ${price}</h6>
-              <h6>size: ${size} sq ft</h6>
+              <h6>size: {size} sq ft</h6>
               <h6>
                 max capacity: {capacity > 1 ? `${capacity} people` : "1 person"}
               </h6>
